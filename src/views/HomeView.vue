165a748de71c8230babc618fceb4f397532e6e4f
DevDuulat <duulat.bekm@mail.ru>
2025-01-28 17:51:30 +0300

    add home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main max-w-screen-xl mx-auto py-20 px-4 sm:px-6 lg:px-8">
+    <HeroSection />
+  </div>
+</template>
+
+<script setup>
+import HeroSection from '@/components/HeroSection.vue'
+
+</script>
+
+<style scoped></style>
